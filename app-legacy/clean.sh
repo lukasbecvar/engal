2a922e7d95c558901082afb9d60149d5f18266b1
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-09-25 11:34:39 +0200

    [-] legacy dir

--- app-legacy/clean.sh
+++
@@ -1,9 +0,0 @@
-# clean backed files
-sudo rm -r backend/build
-sudo rm -r backend/vendor
-sudo rm -r backend/composer.lock
-sudo rm -r backend/GalleryDump.tar.gz
-
-# clean frontend
-sudo rm -r frontend/build
-sudo rm -r frontend/node_modules
--- app-legacy/frontend/src/components/subcomponents/LoadingComponent.js
+++
@@ -1,6 +0,0 @@
-const LoadingComponent = (props) => {
-    
-    return <h1 className="loading">Loading ...</h1>;
-};
-  
-export default LoadingComponent;

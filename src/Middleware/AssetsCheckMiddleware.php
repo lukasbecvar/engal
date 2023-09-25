c59d0aa88dd1548327312df4cd2bac4d0997e891
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-09-25 11:52:02 +0200

    [+] resources build check

---
+++ src/Middleware/AssetsCheckMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Middleware;
+
+/*
+    This middleware check if requird resources is installed
+*/
+
+class AssetsCheckMiddleware
+{
+    public function onKernelRequest(): void
+    {
+        // check if assets is builded
+        if (!file_exists(__DIR__."/../../public/assets/")) {
+            die("Error: assets resources not found, please contact service administrator & report this bug on email: ".$_ENV["ADMIN_EMAIL"]);
+        }
+    }
+}

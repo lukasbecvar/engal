754829eba1a8a6a8d0eec22fe064328b84c501c3
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-12-04 16:13:06 +0100

    [+] added user status to main component

--- frontend/src/components/MainComponent.js
+++ frontend/src/components/MainComponent.js
@@ -1,7 +1,13 @@
+import { useEffect, useState } from "react";
+import LoadingComponent from "./sub-components/LoadingComponent";
+
 function MainComponent() {
 
-    let api_url = localStorage.getItem('api-url');
+    const [loading, setLoading] = useState(true);
 
+    const [username, setUsername] = useState(null);
+
+    let api_url = localStorage.getItem('api-url');
     let user_token = localStorage.getItem('user-token');
 
     async function logout() {
@@ -31,15 +37,54 @@ function MainComponent() {
             // Ošetření chyb při samotném HTTP požadavku
             console.error('Error:', error);
         }
-
     }
+    
+    useEffect(() => {
+        async function fetchData() {
+            if (username == null) {
+                try {
+                    const formData = new FormData();
+                    formData.append('token', user_token);
+        
+                    const response = await fetch(api_url + '/user/status', {
+                        method: 'POST',
+                        body: formData
+                    });
+        
+                    if (!response.ok) {
+                        // Ošetření odmítnuté odpovědi (HTTP status není OK)
+                        console.error('Error:', response.status);
+                        return;
+                    }
+                    const data = await response.json();
+        
+                    if (data.status === 'success') {
+                        setUsername(data.username);
+                    }
+        
+                } catch (error) {
+                    // Ošetření chyb při samotném HTTP požadavku
+                    console.error('Error:', error);
+                }
+            }
+            setLoading(false);
+        }
+    
+        fetchData(); // Okamžité volání asynchronní funkce
+    
+    }, []);
 
-    return (
-        <div>
-            <p>logged: {localStorage.getItem('user-token')}</p>
-            <button type="button" onClick={logout}>Logout</button>
-        </div>
-    );
+    // show loading
+    if (loading == true) {
+        return (<LoadingComponent/>);
+    } else {
+        return (
+            <div>
+                <p>logged: {localStorage.getItem('user-token')}, user: {username}</p>
+                <button type="button" onClick={logout}>Logout</button>
+            </div>
+        );
+    }
 }
 
 export default MainComponent;

b61b2308b77ef0fdf17fbfebed251bb89bb8ec52
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-12-23 13:36:59 +0100

    [+/-] id xd

--- backend/migrations/Version20231223123626.php
+++ backend/migrations/Version20231223123626.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231222130013 extends AbstractMigration
+final class Version20231223123626 extends AbstractMigration
 {
     public function getDescription(): string
     {

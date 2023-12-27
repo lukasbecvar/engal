698ccc857566f8156dc768e4a8bf1e637f97002a
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-12-27 16:58:00 +0100

    [Release] version 3.5

--- backend/migrations/Version20231227154642.php
+++ backend/migrations/Version20231227154642.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231226134332 extends AbstractMigration
+final class Version20231227154642 extends AbstractMigration
 {
     public function getDescription(): string
     {

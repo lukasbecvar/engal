3eb1c9a99e538c0824c20411f6f457da2b778657
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-04-09 17:02:46 +0200

    [-] removed db create script

--- scripts/create-database.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ./backend
-
-# create database
-php bin/console doctrine:database:create --if-not-exists
-php bin/console doctrine:database:create --if-not-exists --env=test
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,6 +2,10 @@
 
 cd ./backend
 
+# create database
+php bin/console doctrine:database:create --if-not-exists
+php bin/console doctrine:database:create --if-not-exists --env=test
+
 # delete old migrations
 rm -rf migrations
 

36c43fa79476411a47da345f42cacdbccc91c92f
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-04-09 17:38:16 +0200

    [R] idk

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# clear
+red_echo () { echo "\033[31m\033[1m$1\033[0m"; }
+
 # clear docker
 sudo rm -rf docker/services
+red_echo "[Clear]: common files clear OK"
 
 # clear frontend
 cd ./frontend
 rm -rf build
 sudo rm -rf node_modules
 rm -rf package-lock.json
+red_echo "[Clear]: frontend files clear OK"
 
 # clear backend
 cd ../backend
@@ -16,3 +21,4 @@ rm -rf var/
 rm -rf vendor/
 rm -rf composer.lock
 rm -rf config/jwt
+red_echo "[Clear]: backend files clear OK"
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+yellow_echo () { echo "\033[33m\033[1m$1\033[0m"; }
+
 # clear terminal history
 clear
 
 # test frontend
 cd ./frontend
+yellow_echo "[Test]: testing Jest"
 # run jest tests
 npx jest
 
 # test backend
 cd ../backend
+yellow_echo "[Test]: testing PHPStan"
 php vendor/bin/phpstan
+yellow_echo "[Test]: testing PHPUnit"
 php bin/phpunit

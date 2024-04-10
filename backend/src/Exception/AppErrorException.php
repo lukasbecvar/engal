3b2c2c92439ab2d8fa649f6186b5f4567c45271d
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-04-10 09:26:31 +0200

    [+] added backend app exception

---
+++ backend/src/Exception/AppErrorException.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * Class AppErrorException
+ * Custom exception class for application errors.
+ *
+ * @package App\Exception
+ */
+class AppErrorException extends HttpException
+{
+    /**
+     * AppErrorException constructor.
+     *
+     * @param string|null     $message  The exception message.
+     * @param int             $code  The http error code.
+     * @param \Throwable|null $previous The previous throwable used for the exception chaining.
+     * @param array<mixed>    $headers  An array of HTTP headers. Each element should be a string representing a header.
+     */
+    public function __construct(string $message = null, int $code, \Throwable $previous = null, array $headers = [])
+    {
+        parent::__construct($code, $message, $previous, $headers);
+    }
+}

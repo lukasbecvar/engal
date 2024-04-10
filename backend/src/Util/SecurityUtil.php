cbf0bc1208959cedf4120ad2f9ccf1224c8eb994
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-04-10 16:05:15 +0200

    [+] added request data escape middleware

---
+++ backend/src/Util/SecurityUtil.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Util;
+
+/**
+ * Class SecurityUtil
+ *
+ * Utility class for security-related operations.
+ *
+ * @package App\Util
+ */
+class SecurityUtil
+{
+    /**
+     * Escape special characters in a string to prevent HTML injection.
+     *
+     * @param string $string The input string to escape.
+     * @return string|null The escaped string or null on error.
+     */
+    public function escapeString(string $string): ?string 
+    {
+        return htmlspecialchars($string, ENT_QUOTES | ENT_HTML5);
+    }
+}
---
+++ backend/tests/Util/SecurityUtilTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests\Util;
+
+use App\Util\SecurityUtil;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SecurityUtilTest
+ * 
+ * Test class for SecurityUtil.
+ * 
+ * @package App\Tests\Util
+ */
+class SecurityUtilTest extends TestCase
+{
+    /**
+     * Test method escapeString().
+     * 
+     * Tests whether the escapeString() method correctly escapes special characters
+     * in a string to prevent HTML injection.
+     */
+    public function testEscapeString(): void
+    {
+        $securityUtil = new SecurityUtil();
+
+        // escape XSS
+        $actual = $securityUtil->escapeString('<script>alert("Hello!");</script>');
+
+        // assert return
+        $this->assertEquals('&lt;script&gt;alert(&quot;Hello!&quot;);&lt;/script&gt;', $actual);
+    }
+}

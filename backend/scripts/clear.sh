2a83764efb69bfc9d00d1e8fa8158a7555ff42d5
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-04-15 10:16:43 +0200

    [R] readme edit

--- backend/scripts/clear.sh
+++ backend/scripts/clear.sh
@@ -6,7 +6,7 @@ red_echo () { echo "\033[31m\033[1m$1\033[0m"; }
 red_echo "[Clear]: clearing backend files"
 php bin/console cache:clear
 rm -rf public/bundles
-rm -rf var/
+sudo rm -rf var/
 rm -rf vendor/
 rm -rf composer.lock
 rm -rf config/jwt

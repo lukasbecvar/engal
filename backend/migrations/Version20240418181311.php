47cbdcc6f6a3c787e3270cd9aeb696fe1366118b
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-04-18 20:15:44 +0200

    [+] fixed logout

--- backend/migrations/Version20240418181311.php
+++ backend/migrations/Version20240418181311.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240418175819 extends AbstractMigration
+final class Version20240418181311 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- frontend/src/component/auth/LogoutComponent.js
+++ frontend/src/component/auth/LogoutComponent.js
@@ -4,31 +4,28 @@ import ErrorMessageComponent from "../sub-component/ErrorMessageComponent";
 export default function LogoutComponent() {
     const [error, setError] = useState(null);
 
-    async function logout() {
+    function logout() {
         let api_url = localStorage.getItem('api-url')
         let login_token = localStorage.getItem('login-token');
 
-        try {
-            const response = await fetch(api_url + '/api/logout', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + login_token
-                }
-            });
-                    
-            const data = await response.json();
+        if (login_token != null) {
+            try {
+                fetch(api_url + '/api/logout', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + login_token
+                    }
+                });
     
-            if (data.status === 'success') {
-                localStorage.removeItem('login-token');
-                window.location.href = '/';
-            } else {
-                setError(data.message);
+                localStorage.removeItem('login-token')
+                window.location.href = '/'
+                
+        
+            } catch (error) {
+                console.error('error:' + error);
+                setError("API connection error");
             }
-    
-        } catch (error) {
-            console.error('error:' + error);
-            setError("API connection error");
         }
     }
 

a831029fea507bddc137bb5203b14796393b23c0
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-05-16 14:52:13 +0200

    [+] added check storage & database integrity

--- backend/src/Manager/StorageManager.php
+++ backend/src/Manager/StorageManager.php
@@ -143,12 +143,24 @@ class StorageManager
      *
      * @param int $ownerId The ID of the owner of the media.
      * @param string $mediaToken The token of the media to check.
+     * @param bool $canCrash The specify if this process can crash without valid response.
+     *
      * @return bool True if the media exists for the given owner, false otherwise.
      */
-    public function isMediaExist(int $ownerId, string $mediaToken): bool
+    public function isMediaExist(int $ownerId, string $mediaToken, bool $canCrash = true): bool
     {
+        // check if entity exist in database
         if ($this->getMediaEntityRepository(['token' => $mediaToken, 'owner_id' => $ownerId]) != null) {
-            return true;
+            // check if media file exist
+            if ($this->getMediaFile($ownerId, $mediaToken, $canCrash) != null) {
+                return true;
+            } else {
+                // check if process can crash before complete
+                if ($canCrash) {
+                    $this->errorManager->handleError('error to get media: ' . $mediaToken . ' but entity exist', 404);
+                }
+                return false;
+            }
         }
 
         return false;
@@ -171,10 +183,11 @@ class StorageManager
      *
      * @param int $userId The ID of the user.
      * @param string $token The token associated with the media file.
+     * @param bool $canCrash The specify if this process can crash without valid response.
      *
      * @return string|null The path of the media file, or null if not found.
      */
-    public function getMediaFile(int $userId, string $token)
+    public function getMediaFile(int $userId, string $token, bool $canCrash = true): ?string
     {
         // build media file path pathern
         $mediaPathPathern = __DIR__ . '/../../storage/' . $_ENV['APP_ENV'] . '/' . $userId . '/*/' . $token . '.*';
@@ -186,7 +199,10 @@ class StorageManager
         if ($files !== false && count($files) > 0) {
             return $files[0];
         } else {
-            $this->errorManager->handleError('error to found media file: ' . $userId . ':' . $token, 404);
+            // check if process can crash before complete
+            if ($canCrash) {
+                $this->errorManager->handleError('error to found media file: ' . $userId . ':' . $token, 404);
+            }
         }
 
         return null;
@@ -393,5 +409,48 @@ class StorageManager
         } catch (\Exception $e) {
             $this->errorManager->handleError('error to preload thumbnails: ' . $e->getMessage(), 500);
         }
+    }
+
+    /**
+     * Retrieve information about all media files.
+     *
+     * This method retrieves information about all media files stored in the system.
+     *
+     * @return array<int<0,max>,array<string,string>> An array containing information about each media file. Each element of the array is an associative array with the following keys:
+     *   - 'folder' (string): The folder where the media file is stored.
+     *   - 'user_id' (string): The ID of the user associated with the media file.
+     *   - 'token' (string): The token identifying the media file, without the file extension.
+     */
+    public function getAllMediaFiles(): array
+    {
+        // build media file path pattern
+        $mediaPathPattern = __DIR__ . '/../../storage/' . $_ENV['APP_ENV'] . '/*/*/*';
+
+        // get files in pattern
+        $files = glob($mediaPathPattern);
+
+        // initialize an empty array to store the extracted information
+        $result = [];
+
+        // iterate through each file path
+        foreach ($files as $file) {
+            // extract token, folder, and user ID using regular expressions
+            preg_match('/\/([^\/]+)\/([^\/]+)\/([^\/]+)$/', $file, $matches);
+
+            // check if the regex match was successful
+            if (isset($matches[1]) && isset($matches[2]) && isset($matches[3])) {
+                // remove extension from token
+                $token = pathinfo($matches[3], PATHINFO_FILENAME);
+
+                // add the extracted information to the result array
+                $result[] = [
+                    'folder' => $matches[2],
+                    'user_id' => $matches[1],
+                    'token' => $token
+                ];
+            }
+        }
+
+        return $result;
     }
 }

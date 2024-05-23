6061d383ada6adcb0a11ac8f28f8f29eb79b19ef
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-05-23 23:02:46 +0200

    [+] added todo

--- backend/migrations/Version20240523204946.php
+++ backend/migrations/Version20240523204946.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240523195715 extends AbstractMigration
+final class Version20240523204946 extends AbstractMigration
 {
     public function getDescription(): string
     {
